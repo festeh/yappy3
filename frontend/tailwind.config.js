1f49c250912b06688cb003c628e592796467a0cc
Dima Lipin <dimich3d@ya.ru>
2024-10-30 20:59:52 +0100

    tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ["./index.html", './src/**/*.{svelte,js,ts}'], //for unused css
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
