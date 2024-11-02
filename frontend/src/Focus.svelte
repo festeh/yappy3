3c758edd9126f64e11fb65111bc2c01794ae9737
Dima Lipin <dimich3d@ya.ru>
2024-11-02 17:23:14 +0100

    upd

--- frontend/src/Focus.svelte
+++ frontend/src/Focus.svelte
@@ -29,9 +29,11 @@
 
 <main class="h-screen w-full">
 	<div class="flex h-full flex-col items-center justify-center gap-8">
-		<div class="text-center text-4xl font-bold text-gray-300">focusing...</div>
-		<button class="rounded-lg bg-gray-700 px-6 py-3 text-xl text-gray-300 hover:bg-gray-600">
+		<div class="text-center text-4xl font-bold text-gray-300">
 			Focus: {focusing}
+		</div>
+		<button class="rounded-lg bg-gray-700 px-6 py-3 text-xl text-gray-300 hover:bg-gray-600">
+			Focus Now
 		</button>
 	</div>
 </main>
