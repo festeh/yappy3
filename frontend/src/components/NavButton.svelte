5ea0be028f0d2248c6c98e2830acd3723bfef661
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-03 21:39:39 +0100

    style: Increase text size and center in NavButton

--- frontend/src/components/NavButton.svelte
+++ frontend/src/components/NavButton.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <button
-  class="flex items-center justify-center rounded-lg text-2xl transition-colors {currentView === view
+  class="flex items-center justify-center rounded-lg text-4xl p-1 text-center transition-colors {currentView === view
     ? 'bg-blue-100 text-blue-600'
     : 'hover:bg-gray-100'}"
   on:click={onClick}
